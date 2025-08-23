248a36dafacf20e882e2a51da2d62b8c9ff7b9db
Amandeep Singh <amandeepaluna1@gmail.com>
2025-08-23 16:43:50 +0530

    make localhost compatible

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,5 @@
 // export const BASE_URL = "/api"
 //export const FRONT_URL = "http://localhost:5173"
-export const FRONT_URL = "https://pusle-match.onrender.com"
-export const BASE_URL = "https://tinderinspired-2.onrender.com"
+export const FRONT_URL = location.hostname === "localhost"? "http://localhost:5173": "https://pusle-match.onrender.com";
+
+export const BASE_URL = location.hostname === "localhost"? "http://localhost:3000": "https://tinderinspired-2.onrender.com";
